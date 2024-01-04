846575056bf956f9c2890c231a24b8330dacaae9
mariy <mariy@MITKO>
2024-01-04 22:26:10 +0300

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,27 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+var matrix1 = DenseMatrix.OfArray(new double[,]
+{
+    { 2, -1, 3 },
+    { 4, 1, 0 },
+    { -3, 2, 5 }
+});
+var matrix2 = DenseMatrix.OfArray(new double[,]
+{
+    { 1, 0, -2 },
+    { 3, -4, 2 },
+    { -1, 5, 7 }
+});
+
+var multiplyResult = matrix1 * 3;
+Console.WriteLine($"{multiplyResult}");
+
+var additionResult = matrix1 + matrix2;
+Console.WriteLine($"\n{additionResult}");
+
+var determinant = matrix1.Determinant();
+Console.WriteLine($"{determinant:F0}");
+
+var solve = matrix1.Solve(matrix1);
+Console.WriteLine($"\n{solve}");
